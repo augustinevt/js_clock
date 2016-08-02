109442231ab5fe33bd715768af207f93a79b5597
Augustine von Trapp <Augustvontrapp@gmail.com>
2016-08-02 16:35:40 -0700

    all but the alarm function

--- js/hello-interface.js
+++ js/hello-interface.js
@@ -1,7 +1,17 @@
 $(function() {
 
-  $('body').click(function() {
-    console.log('it works');
+
+    setInterval(function(){
+      $('#time').text(moment());
+    }, 1000);
+
+
+  $('#make-alarm').click(function() {
+    var date = $('#date').val();
+    var hour = $('#hour').val();
+    var minute = $('#minute').val();
+    alarm = moment(date).hour(hour).minute(minute).format();
+    $('#alarm-list').append('<li> alarm at: '+alarm+'</li>');
   });
 
 });
